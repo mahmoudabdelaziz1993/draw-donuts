0692e9e4662cc3e23ceb09e2fe6c0fa93c07dc95
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2020-09-12 19:55:30 +0200

    landing page

--- front-end/src/components/pages/Landing.js
+++ front-end/src/components/pages/Landing.js
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import { Box, Typography, Button, makeStyles } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+const useStyles = makeStyles({
+    root: {
+        width: "100%",
+        height: "70vh",
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    sub: {
+        marginTop: "87px",
+        width: "60%",
+        flexDirection: 'column',
+        justifyContent: 'center'
+    }
+})
 function Landing() {
+    const classes = useStyles()
     return (
-        <div>
-            <h1> Landing</h1>
-        </div>
+        <Box className={classes.root}>
+            <Box className={classes.sub} >
+                <Typography variant='h1'> Create you Donut chart Today </Typography>
+                <Link to='/signin'>
+                    <Button variant='contained' color="primary"  >Start</Button>
+                </Link>
+            </Box>
+        </Box>
     )
 }
 
